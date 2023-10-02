3d6056f45ba2a64d08bb824abdae8e24ebe37b02
Samfield Hawb <samfield4sure@gmail.com>
2023-10-02 01:50:46 +0000

    fix betty error

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,13 @@
 
 void _puts_recursion(char *s)
 {
-    if (*s)
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-    else
-    {
-        _putchar('\n');
-    }
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+{
+_putchar('\n');
+}
 }
