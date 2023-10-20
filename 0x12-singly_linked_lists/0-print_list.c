fd65e4fb3219af7dc7cdf4a8de43310db9ad1e2d
Samfield Hawb <samfield4sure@gmail.com>
2023-10-20 07:40:43 +0000

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list - print all members of the list
+ * @h: points to node
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+unsigned int i = 0;
+
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+i++;
+h = h->next;
+}
+return (i);
+}
