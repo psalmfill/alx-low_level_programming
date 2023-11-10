c23d46e4d30c47431caef70e176d1beacf3abbb0
Samfield Hawb <samfield4sure@gmail.com>
2023-11-10 01:23:04 +0000

    1-print_binary.c

--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,1 +1,2 @@
 0-binary_to_uint.c - a function that converts a binary number to an unsigned int.
+1-print_binary.c - a function that prints the binary representation of a number.
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,4 +2,6 @@
 #define MAIN_H
 unsigned long int _power(unsigned int base, unsigned int pow);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
 #endif /*MAIN_H*/
