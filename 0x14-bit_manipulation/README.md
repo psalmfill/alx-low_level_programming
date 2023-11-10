e533ff5e7df63c14ce6f5bf1e2967fd51feac3d8
Samfield Hawb <samfield4sure@gmail.com>
2023-11-10 01:26:20 +0000

    2-get_bit.c

--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,2 +1,3 @@
 0-binary_to_uint.c - a function that converts a binary number to an unsigned int.
 1-print_binary.c - a function that prints the binary representation of a number.
+2-get_bit.c - a function that returns the value of a bit at a given index.
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,5 +3,6 @@
 unsigned long int _power(unsigned int base, unsigned int pow);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char c);
 #endif /*MAIN_H*/
