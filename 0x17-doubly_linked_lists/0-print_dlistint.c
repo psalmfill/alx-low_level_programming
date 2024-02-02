be2b924a6ef0dec863007d7b1f061d9ae0f6d67a
Samfield Hawb Bassey <samfield4sure@gmail.com>
2024-02-02 01:17:36 +0100

    fixed formating

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,13 +9,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    unsigned int count = 0;
+	unsigned int count = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
-    return (count);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
