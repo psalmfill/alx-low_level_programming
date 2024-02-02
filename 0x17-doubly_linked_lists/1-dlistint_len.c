7e24e4a6965192e8e649f8800794e315085ccccf
Samfield Hawb Bassey <samfield4sure@gmail.com>
2024-02-02 01:19:52 +0100

    fixed formating

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -10,20 +10,20 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    unsigned int count = 0;
-    const dlistint_t *list;
+	unsigned int count = 0;
+	const dlistint_t *list;
 
-    list = malloc(sizeof(dlistint_t));
-    list = h;
+	list = malloc(sizeof(dlistint_t));
+	list = h;
 
-    if (list == NULL)
-    {
-        return (0);
-    }
-    while (list)
-    {
-        list = list->next;
-        count++;
-    }
-    return (count);
+	if (list == NULL)
+	{
+		return (0);
+	}
+	while (list)
+	{
+		list = list->next;
+		count++;
+	}
+	return (count);
 }
